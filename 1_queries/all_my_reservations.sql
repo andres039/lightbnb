786687c16841dfe51802c9c8319d1c64192ac91e
Andres <andres039039@hotmail.com>
2021-11-25 15:36:55 -0500

    adds reservations functionality

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -7,3 +7,13 @@ WHERE properties.owner_id = 1 AND end_date < now()::date
 GROUP BY reservations.id, properties.id
 ORDER BY start_date 
 LIMIT 10;
+
+SELECT (reservations.*), (properties.*), AVG(property_reviews.rating) as average_rating 
+FROM properties 
+JOIN reservations ON reservations.property_id = properties.id
+JOIN property_reviews ON 
+reservations.id = property_reviews.reservation_id
+WHERE properties.owner_id = 3 AND end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY start_date 
+LIMIT 10;
--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -102,8 +102,20 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function (guest_id) {
+ // return getAllProperties(null, 2);
+ return pool
+ .query(`SELECT (reservations.*), (properties.*), AVG(property_reviews.rating) as average_rating 
+ FROM properties 
+ JOIN reservations ON reservations.property_id = properties.id
+ JOIN property_reviews ON 
+ reservations.id = property_reviews.reservation_id
+ WHERE properties.owner_id = $1 AND end_date < now()::date
+ GROUP BY reservations.id, properties.id
+ ORDER BY start_date 
+ LIMIT 10;`, [guest_id])
+ .then((result) => result)
+ .catch((err) => err)
 };
 exports.getAllReservations = getAllReservations;
 
