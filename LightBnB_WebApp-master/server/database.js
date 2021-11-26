756d36c5ac5f6e4e9637a89ac6701c40205668db
Andres <andres039039@hotmail.com>
2021-11-25 19:43:51 -0500

    adds insert new listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -134,7 +134,7 @@ exports.getAllReservations = getAllReservations;
 // }
 
 const getAllProperties = (options, limit) => {
-  console.log(options)
+  
   const queryParams = [];
 
   let queryString = `
@@ -151,7 +151,7 @@ const getAllProperties = (options, limit) => {
 
   if (options.owner_id) {
     queryParams.push(+options.owner_id);
-    queryString += `AND owner_id = $${queryParams.length}`;
+    queryString += `AND properties.owner_id = $${queryParams.length}`;
   }
 
   if (options.minimum_price_per_night) {
@@ -194,9 +194,27 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
+
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+  let queryString = `
+  INSERT INTO properties
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `
+
+  console.log(queryString, queryParams);
+  
+  return pool
+  .query(queryString, queryParams)
+  .then((res) => res.rows)
+  .catch((err) => err)
+  /*
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
+  */
 };
 exports.addProperty = addProperty;
